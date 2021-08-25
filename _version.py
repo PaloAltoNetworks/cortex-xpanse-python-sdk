3cac110af705f7b5e51a178526c8ae8955e08b7a
cachukwu <cachukwu@paloaltonetworks.com>
2021-08-25 07:30:19 -0400

    GNY-223
    Github Release actions

---
+++ _version.py
@@ -0,0 +1,11 @@
+"""
+Provides cortex-xpanse-python-sdk version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update cortex-xpanse-python-sdk` to change this file.
+
+from incremental import Version
+
+__version__ = Version("cortex-xpanse-python-sdk", 21, 8, 0)
+__all__ = ["__version__"]
