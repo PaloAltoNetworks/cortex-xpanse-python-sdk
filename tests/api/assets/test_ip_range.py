20ca2513e1e869b2dffa1632090cc60dc7ca8419
Hilary Lynch <hlynch@paloaltonetworks.com>
2021-11-23 09:17:04 -0500

    GNY-322 tests wip

--- tests/api/assets/test_ip_range.py
+++ tests/api/assets/test_ip_range.py
@@ -14,15 +14,15 @@ def test_assets_ip_range_list(api):
 
 @pytest.mark.vcr()
 def test_assets_ip_range_get(api):
-    ip_range = api.assets.ip_range.v2.get("75ebc9d8-db03-4c00-b44b-db458d72c817")
+    ip_range = api.assets.ip_range.v2.get("07a03fce-df64-38ee-a024-1ea84d2df18c")
     assert isinstance(ip_range, dict)
 
 
 @pytest.mark.vcr()
 def test_assets_ip_range_create(api):
-    start = "2.117.113.128"
+    start = "2.117.113.130"
     end = "2.117.113.135"
-    parent_id = "09ce0303-94e5-4037-8226-e708b6e4874f"
+    parent_id = "01dd9d37-b0fe-3014-a17a-7db042383a00"
     ip_range = api.assets.ip_range.v2.create(start, end, parent_id)
     assert isinstance(ip_range, dict), "Expected instance of `dict` to be returned."
     assert ip_range["id"] is not None
@@ -34,9 +34,9 @@ def test_assets_ip_range_create(api):
 
 @pytest.mark.vcr()
 def test_assets_ip_range_delete(api):
-    start = "2.117.113.128"
+    start = "2.117.113.130"
     end = "2.117.113.135"
-    parent_id = "09ce0303-94e5-4037-8226-e708b6e4874f"
+    parent_id = "01dd9d37-b0fe-3014-a17a-7db042383a00"
     ip_range = api.assets.ip_range.v2.create(start, end, parent_id)
     assert ip_range["id"] is not None
     assert api.assets.ip_range.v2.delete(ip_range["id"])
@@ -47,25 +47,25 @@ def test_assets_ip_range_delete(api):
 
 @pytest.mark.vcr()
 def test_assets_ip_range_update(api):
-    start = "2.117.113.128"
+    start = "2.117.113.130"
     end = "2.117.113.135"
-    parent_id = "09ce0303-94e5-4037-8226-e708b6e4874f"
+    parent_id = "01dd9d37-b0fe-3014-a17a-7db042383a00"
     ip_range = api.assets.ip_range.v2.create(start, end, parent_id)
     assert ip_range["id"] is not None
     new_range = api.assets.ip_range.v2.update(
         ip_range["id"], endAddress="2.117.113.131"
     )
-    assert new_range["startAddress"] == "2.117.113.128"
+    assert new_range["startAddress"] == "2.117.113.130"
     assert new_range["endAddress"] == "2.117.113.131"
     assert api.assets.ip_range.v2.delete(ip_range["id"]), "Error cleaning up IP range"
 
 
 @pytest.mark.vcr()
 def test_assets_ip_range_tag(api):
-    start = "2.117.113.128"
+    start = "2.117.113.130"
     end = "2.117.113.135"
-    parent_id = "09ce0303-94e5-4037-8226-e708b6e4874f"
+    parent_id = "01dd9d37-b0fe-3014-a17a-7db042383a00"
     ip_range = api.assets.ip_range.v2.create(start, end, parent_id)
     assert ip_range["id"] is not None
-    assert api.assets.ip_range.v2.tag(ranges=[ip_range["id"]], tags=["test_tag"])
+    assert api.assets.ip_range.v2.tag(ranges=[ip_range["id"]], tags=["sdk_test_cc"])
     assert api.assets.ip_range.v2.delete(ip_range["id"]), "Error cleaning up IP range"
--- tests/unit/test_client.py
+++ tests/unit/test_client.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 
 from xpanse.client import ExClient
 from xpanse.error import UnexpectedValueError
@@ -7,13 +8,22 @@ from xpanse.error import UnexpectedValueError
 @pytest.mark.vcr()
 def test_ExClient_invalid_bearer_token():
     with pytest.raises(UnexpectedValueError) as e:
-        ExClient(bearer_token="wrong-token")
+        ExClient(bearer_token="wrong-token", url="https://expander.staging.qadium.com")
     assert "Could not get an id token based on the given refresh token" in str(e)
 
 
 @pytest.mark.vcr()
 def test_ExClient_refresh_jwt():
-    api = ExClient(jwt="expired-jwt", bearer_token="bearer-token")
+    api = ExClient(jwt=os.getenv("XPANSE_TEST_EXPIRED_JWT", "wwwwwwwwwwwwwwwwwwwwwwwwwwwww"),
+                   bearer_token=os.getenv("XPANSE_BEARER_TOKEN", "wwwwwwwwwwwwwwwwwwwwwwww"))
     resp = api.get("api/v2/assets/domains/count")
-    assert resp
-    assert api._jwt == "new-jwt"
+    assert resp.status_code == 200
+
+# @pytest.mark.vcr()
+# def test_ExClient_refresh_jwt_cc():
+#     api = ExClient(jwt=os.getenv("XPANSE_TEST_EXPIRED_JWT", "wwwwwwwwwwwwwwwwwwwwwwwwwwwww"),
+#                    client_id=os.getenv("XPANSE_TEST_CLIENT_ID", "wwwwwwwwwwwwwwwwwwwwwwwwwwwww"),
+#                    client_secret=os.getenv("XPANSE_TEST_CLIENT_SECRET", "wwwwwwwwwwwwwwwwwwwwwwwwwwwww"),
+#                    url="https://expander.staging.qadium.com")
+#     resp = api.get("api/v2/assets/domains/count")
+#     assert resp.status_code == 200
