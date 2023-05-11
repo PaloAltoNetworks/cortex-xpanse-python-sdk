37df0d66a86be5782a7f98d7462a5f47ccfbd93d
Benjamin Daghir <benjamin.daghir@expanseinc.com>
2023-05-11 14:11:48 -0400

    Revert rename

--- xpanse/utils.py
+++ xpanse/utils.py
@@ -3,16 +3,16 @@ from typing import Any, Dict, List, Optional
 from xpanse.const import PublicApiFields, DEFAULT_REQUEST_PAYLOAD_FIELD
 
 
-def normalize_param_names(request_data: Dict[str, Any]):
+def normalize_param_names(kwargs: Dict[str, Any]):
     """
     Replace underscores in kwarg names with hyphens.
     """
     normalized_args = {}
-    if "params" in request_data:
-        for k, v in request_data.get("params", {}).items():
+    if "params" in kwargs:
+        for k, v in kwargs.get("params", {}).items():
             normalized_args[k.replace("_neq", "!").replace("_", "-")] = v
-        request_data["params"] = normalized_args
-    return request_data
+        kwargs["params"] = normalized_args
+    return kwargs
 
 
 def build_request_payload(
