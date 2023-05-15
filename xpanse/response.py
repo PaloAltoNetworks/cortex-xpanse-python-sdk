1bf1775acd67a1d2c458bc7c21c2e7aee5209311
Benjamin Daghir <benjamin.daghir@expanseinc.com>
2023-05-15 14:18:39 -0400

    Add doc strings

--- xpanse/response.py
+++ xpanse/response.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Any
 
 from requests import Response
 
@@ -6,16 +6,25 @@ from xpanse.const import PublicApiFields
 
 
 class XpanseResponse:
+    """
+    This class wraps the existing requests.Response class to enable ease of parsing while maintaining the
+    raw response object and status code.
+
+    Usages:
+        > To see the raw response from the request, access the "response" attribute: xpanse_response.response
+        > To grab the parsed data from the response, access the "data" attribute: xpanse_response.data
+    """
+
     def __init__(self, response: Response, data_key: Optional[str] = None):
         self._response = response
         self._data_key = data_key
 
     @property
-    def response(self):
+    def response(self) -> Response:
         return self._response
 
     @property
-    def data(self):
+    def data(self) -> Any:
         response_data = self.response.json()
         if self._data_key is not None and self._data_key in response_data.get(
             PublicApiFields.REPLY, {}
