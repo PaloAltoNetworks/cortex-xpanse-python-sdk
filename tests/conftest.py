832f554d7a249ac91af369e4c4835bb82ad9d6c7
Benjamin Daghir <benjamin.daghir@expanseinc.com>
2023-05-15 16:13:05 -0400

    Cleanup

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,36 +22,3 @@ def api():
         api_key_id=os.getenv("TEST_CORTEX_API_KEY_ID", 1),
         api_key=os.getenv("TEST_CORTEX_API_KEY", "wwwwwwwwwwwwwwwwwwwwwwwwwwwww"),
     )
-
-
-if __name__ == '__main__':
-    client = XpanseClient(url='https://ben-expander.crtx-qa2-uat.us.paloaltonetworks.com/',
-                          api_key_id=2,
-                          api_key='vtyF031c5Gpboh80P657Ocy8UcbTX01mDtZXdim3miIVSyhL1QAs3k5F2Rq0DEHVNzCWR7G4kyONsyKjAnqWWvwgDO3bRTzraZ9PMho8ZbiR5SEeY9K5Fd6SllfBoNkv')
-
-    api = client.assets
-    list = api.list(request_data={"search_from": 0, "search_to": 1})
-    print(list.next())
-    ids = ["2561688d-b632-3681-9cb7-cf49b19de09a"]
-    get = api.get(asset_ids=ids)
-    print(get.data)
-    count = api.count()
-    print(count.data)
-
-    api = client.external_ip_ranges
-    list = api.list(request_data={"search_from": 0, "search_to": 1})
-    print(list.next())
-    ids = ["229bdac6-1641-346a-be24-342589903bb5"]
-    get = api.get(ip_range_ids=ids)
-    print(get.data)
-    count = api.count()
-    print(count.data)
-
-    api = client.services
-    list = api.list(request_data={"search_from": 0, "search_to": 1})
-    print(list.next())
-    ids = ["a2da74a2-5adb-3c44-bc03-bd8a7a3b1085"]
-    get = api.get(service_ids=ids)
-    print(get.data)
-    count = api.count()
-    print(count.data)
