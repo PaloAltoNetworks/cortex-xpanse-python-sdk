8d309d13bebfe8a0c1a53402ce4a4b9608e678d2
ben-expanse <75336435+ben-expanse@users.noreply.github.com>
2023-05-17 15:24:57 -0400

    [EXPANDR-2602] Add Attack Surface Rules Endpoint to SDK (#130)
    * Create XpanseClient, XpanseIterator, XpanseEndpoint, testing, and stub out specific API classes
    
    * Generalize XpanseResultIterator kwargs
    
    * Update tests
    
    * Add IncidentsApi
    
    * Use later versions of Github actions
    
    * Drop Python Version 3.6, Add Version 3.9 and 3.10
    
    * Escape 3.10 with quotes?
    
    * Lint!
    
    * Lint
    
    * Fix tests
    
    * Revert rename
    
    * Better type checking
    
    * Remove V3 endpoint
    
    * Implement AlertsApi
    
    * Create an XpanseResponse object
    
    * Add doc strings
    
    * Update tests
    
    * Lintttt
    
    * Update doc strings
    
    * Create AssetsManagementV1 APIs
    
    * Cleanup
    
    * Refactor to optimize queries, include asset_types as a parameter
    
    * Better logic organization
    
    * Better logic AssetsManagementV1organization
    
    * remove test code
    
    * Fix enum value
    
    * Add Attack Surface Rule endpoint
    
    * Lint
    
    * Add test cassette files
    
    * Rename test files
    
    * Readd test

--- xpanse/api/attack_surface_rules/v1/attack_surface_rules.py
+++ xpanse/api/attack_surface_rules/v1/attack_surface_rules.py
@@ -1,21 +1,45 @@
 from typing import Any, List
 
+from xpanse.const import V1_PREFIX
 from xpanse.endpoint import XpanseEndpoint
-from xpanse.iterator import XpanseResultIterator
+from xpanse.response import XpanseResponse
+from xpanse.utils import build_request_payload
 
 
-# TODO:// Implement Attack Surface Rules https://jira-hq.paloaltonetworks.local/browse/EXPANDR-2602
 class AttackSurfaceRulesApi(XpanseEndpoint):
     """
     Part of the Public API for handling Attack Surface Rules.
     See: TODO:// https://jira-hq.paloaltonetworks.local/browse/EXPANDR-3062
     """
 
-    def list(self, **kwargs: Any) -> XpanseResultIterator:
-        raise NotImplementedError()
+    ENDPOINT = f"{V1_PREFIX}/get_attack_surface_rules/"
+    DATA_KEY = "attack_surface_rules"
 
-    def get(self, rule_ids: List[str], **kwargs: Any) -> Any:
-        raise NotImplementedError()
+    def list(self, request_data: Any = None, **kwargs: Any) -> XpanseResponse:
+        kwargs = build_request_payload(request_data=request_data, **kwargs)
+        response = self._api.post(path=self.ENDPOINT, **kwargs)
+        return XpanseResponse(response, self.DATA_KEY)
 
-    def count(self, **kwargs: Any) -> int:
-        raise NotImplementedError()
+    def get(
+        self,
+        attack_surface_rule_ids: List[str],
+        request_data: Any = None,
+        **kwargs: Any,
+    ) -> XpanseResponse:
+        filters = [
+            {
+                "field": "attack_surface_rule_id",
+                "operator": "in",
+                "value": attack_surface_rule_ids,
+            }
+        ]
+        kwargs = build_request_payload(
+            request_data=request_data, filters=filters, **kwargs
+        )
+        response = self._api.post(self.ENDPOINT, **kwargs)
+        return XpanseResponse(response, data_key=self.DATA_KEY)
+
+    def count(self, request_data: Any = None, **kwargs: Any) -> XpanseResponse:
+        return super(AttackSurfaceRulesApi, self)._count(
+            self.ENDPOINT, request_data=request_data, **kwargs
+        )
